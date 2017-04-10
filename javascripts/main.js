10abebe8c3157465b6fe0771319a0b25608e2c56
sharon smith <sharon.e.smith2@gmail.com>
2017-04-09 23:38:43 -0500

    working through button click

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,17 @@
 
-console.log("running main.js");
+//***************************************************
+// event listener for <Print Array> button
+//***************************************************
+$("#print").click(function (){
+
+  $("#print").addClass("hidden");
+
+  // $("#pPuffGirls").addClass("hidden");
+  // $("#pPuffFighting").removeClass("hidden");
+  // $("#pPuffFighting").addClass("visible");
+  // $("#action").removeClass("hidden");
+  // $("#action").addClass("visible");
+
+});
+
+
